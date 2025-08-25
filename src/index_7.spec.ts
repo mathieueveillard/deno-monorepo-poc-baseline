7400a6018d543fe52328038912dd8c4493e20799
mathieueveillard <mathieu.eveillard@gmail.com>
2025-08-25 17:42:38 +0200

    CI: test with 50000 tests

---
+++ src/index_7.spec.ts
@@ -0,0 +1,59 @@
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
+
+for (let i = 0; i < 500; i++) {
+  test("A simple test (Jest)", () => {
+    expect(1 + 1).toEqual(2);
+  });
+}
